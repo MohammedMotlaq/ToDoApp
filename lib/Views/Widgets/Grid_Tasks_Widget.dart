1d38c684c2c60e75ada75ba81b8e429ebe8e4b81
MAZODA <oabumutlak@gmail.com>
2023-04-10 17:45:14 +0300

    Add: Grid for Tsk Widgets

---
+++ lib/Views/Widgets/Grid_Tasks_Widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class GridTaskWidget extends StatefulWidget {
+  const GridTaskWidget({super.key});
+
+  @override
+  State<GridTaskWidget> createState() => _GridTaskWidgetState();
+}
+
+class _GridTaskWidgetState extends State<GridTaskWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // padding: EdgeInsets.symmetric(vertical: 7.h, horizontal: 11.w),
+      padding: EdgeInsets.only(top: 21.h, bottom: 7.h,left:9.w ,right:12 ),
+
+      margin: EdgeInsets.only(top: 23.h),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.r),
+        color: const Color.fromRGBO(255, 68, 68, 1),
+      ),
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Pay Emma",
+              style: TextStyle(
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Roboto",
+                  color: Colors.white),
+            ),
+          ),
+          SizedBox(
+            height: 22.h,
+          ),
+          Text(
+            "20 dollars for manga",
+            style: TextStyle(
+                fontSize: 16.sp,
+                color: Colors.white,
+                fontWeight: FontWeight.w300),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ImageIcon(
+                const AssetImage("assets/icons/check-square.png"),
+                color: Colors.white,
+                size: 28.h,
+              ),
+              ImageIcon(
+                const AssetImage("assets/icons/trash.png"),
+                color: Colors.white,
+                size: 28.h,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
