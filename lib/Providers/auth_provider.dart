b2cae3f79a6601fe75e39f821cdd16b345c14943
MAZODA <oabumutlak@gmail.com>
2023-04-11 23:18:07 +0300

    Error page snackbar

--- lib/Providers/auth_provider.dart
+++ lib/Providers/auth_provider.dart
@@ -4,6 +4,7 @@ import 'package:rounded_loading_button/rounded_loading_button.dart';
 import 'package:to_do_app/Helpers/Auth_Helper.dart';
 import 'package:to_do_app/Router/App_Router.dart';
 import 'package:to_do_app/Views/Auth/SignIn_Screen.dart';
+import 'package:to_do_app/Views/Auth/SignUp_Screen.dart';
 import 'package:to_do_app/Views/Splash/Splash.dart';
 
 class AuthProvider extends ChangeNotifier {
@@ -11,10 +12,16 @@ class AuthProvider extends ChangeNotifier {
   bool? isLoading;
   RoundedLoadingButtonController btnController =
       new RoundedLoadingButtonController();
+
   signUp(String email, String name, String password,
       String confirmPassword) async {
     signedUp = await AuthHelper.authHelper
         .signUp(email, name, password, confirmPassword);
+    if (signedUp!) {
+      AppRouter.pushWidget(const SignInScreen());
+    } else {
+      AppRouter.showErrorSnackBar("Email Already Exists!");
+    }
     notifyListeners();
   }
 
@@ -25,7 +32,7 @@ class AuthProvider extends ChangeNotifier {
       notifyListeners();
       Future.delayed(const Duration(seconds: 2), () async {
         AppRouter.popAll();
-        AppRouter.pushWithReplacment(const SignInScreen());
+        AppRouter.pushWithReplacment(const SignUpScreen());
       });
     } else {
       Future.delayed(const Duration(seconds: 1), () async {
